4dc2c7096ca8adc049a953bbcd8c71e1c3535648
Jannik Kollmann <kollmann_jannik@yahoo.de>
2016-10-30 18:47:10 +0100

    Revert accidental change

--- fetcher/models/application.js
+++ fetcher/models/application.js
@@ -3,12 +3,23 @@ const db = require('../services/db');
 
 /**
  * Store the applicationId
-* @param {string} applicationId
-* @returns {Promise}
-*/
+ * @param {string} applicationId
+ * @returns {Promise}
+ */
 function insert(applicationId) {
-	logger.info(`Inserting datastream for ${applicationId}`);
+	logger.info(`Inserting applicationId ${applicationId} into table application`);
 	const sql = 'INSERT INTO application (id) VALUES ($1::uuid)';
+	return db.query(sql, [applicationId]);
+}
+
+/**
+ * Select the application
+ * @param {string} applicationId
+ * @returns {Promise}
+ */
+function get(applicationId){
+	logger.info(`Getting applicationId ${applicationId} from table application`);
+	const sql = 'SELECT * FROM application WHERE id = $1::uuid';
 	return db.query(sql, [applicationId]);
 }
 
